0007e81883b458d2a03fa3a7ede6c8c837d28c5e
HelloImFranky <47162800+HelloImFranky@users.noreply.github.com>
2022-01-14 16:13:26 -0500

    welcome view created, main.dart edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,31 @@
+import 'package:anAbundanceOfCats/view/welcome_view.dart';
 import 'package:flutter/material.dart';
 import 'view/page_allbreeds_choice_page.dart';
 import 'view/page_breed_choice_image_results_.dart';
 import 'view/page_onerandomcat_result.dart';
-import 'view/widgets/button_widgets/button_random_cat_frontpage.dart';
-import 'view/widgets/button_widgets/button_search_frontpage.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: '/',
-    routes: {
-      '/': (context) => const WelcomePage(),
-      '/searchPage': (context) => const SearchPage(),
-      '/resultPage': (context) => const ResultPage(),
-      '/breedResultPage': (context) => const BreedResultPage(),
-    },
-    theme: ThemeData.dark(),
-  ));
+  runApp(const MyApp());
 }
 
-class WelcomePage extends StatefulWidget {
-  const WelcomePage({Key? key}) : super(key: key);
-  @override
-  _WelcomePage createState() => _WelcomePage();
-}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-class _WelcomePage extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text('Image Search Engine')),
-        body: SafeArea(
-            child: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                RandomCatFPElevatedButton(),
-                SizedBox(height: 50,),
-                SearchFPElevatedButton(),
-              ]),
-        )));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const WelcomePage(),
+        '/searchPage': (context) => const SearchPage(),
+        '/resultPage': (context) => const ResultPage(),
+        '/breedResultPage': (context) => const BreedResultPage(),
+      },
+      theme: ThemeData.dark(),
+    );
   }
 }
+
+
+
---
+++ lib/view/welcome_view.dart
@@ -0,0 +1,25 @@
+import 'package:anAbundanceOfCats/view/widgets/button_widgets/button_random_cat_frontpage.dart';
+import 'package:anAbundanceOfCats/view/widgets/button_widgets/button_search_frontpage.dart';
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Image Search Engine')),
+        body: SafeArea(
+            child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                RandomCatFPElevatedButton(),
+                SizedBox(
+                  height: 50,
+                ),
+                SearchFPElevatedButton(),
+              ]),
+        )));
+  }
+}
